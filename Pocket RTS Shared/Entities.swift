f265c3bf877ce4483d19795425ca0b6dc37e8140
Erick Sanchez <esericksanc@gmail.com>
2021-11-14 22:35:38 -0800

    WIP

---
+++ Pocket RTS Shared/Entities.swift
@@ -0,0 +1,42 @@
+////
+////  Entities.swift
+////  Pocket RTS iOS
+////
+////  Created by Erick Sanchez on 11/12/21.
+////
+//
+//import GameplayKit
+//
+//
+//class Base: GKEntity {
+//    
+//}
+//
+//class Health: GKComponent {
+//    
+//}
+//
+//class UnitIdle: GKState {
+//}
+//class MovingIdle: GKState {
+//}
+//class DefendingIdle: GKState {
+//}
+//class MarchingIdle: GKState {
+//}
+//class AttackingIdle: GKState {
+//}
+//
+//// Worker, solider, anything that can move by a player
+//class Unit: GKEntity {
+//    var state: GKStateMachine
+//    
+//}
+//
+//class Move: GKComponent {
+//    
+//}
+//
+//class GameSceneState {
+//    // is the player in the home screen, pause menu, game
+//}
